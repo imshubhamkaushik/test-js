213f53f31fc3856a4a167c4717ec133d13b4c00e
Shubham <shubhamkaushik91@gmail.com>
2023-10-06 12:08:30 +0530

    Updated

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) //we are using nodejs, not browser
+
+let name = "me"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+console.log(typeof "ho");
+console.log(typeof age);
+console.log(typeof null)
+console.log(typeof undefined)
