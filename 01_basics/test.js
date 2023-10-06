435ee3e1ce9a88f1dbc8ed63285e056718890ada
Shubham Kaushik <41125334+imshubhamkaushik@users.noreply.github.com>
2023-10-06 06:15:42 +0000

    Updated

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,21 @@
 console.log("HO");
+const accountId=144553;
+let accountEmail = "me@me.com"
+var accountPassword = "12121212"
+accountCity = "Jaipur"
+
+let accountState ;
+// accountId = 2 // not allowed
+
+accountEmail="h@h.c"
+accountPassword="21212"
+accountCity="b"
+/*
+Prefer not to use
+because of issue in block scope and functional scope
+*/
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
+
+console.log(accountId);
