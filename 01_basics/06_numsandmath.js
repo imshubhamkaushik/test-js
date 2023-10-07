7de292d025b89cb943f63f14baf1aa7b72b639d8
Shubham <shubhamkaushik91@gmail.com>
2023-10-07 18:21:46 +0530

    7/Oct

--- 01_basics/06_numsandmath.js
+++ 01_basics/06_numsandmath.js
@@ -1,5 +1,26 @@
 const score = 400
-console.log(score);
+//console.log(score);
 
 const balance  = new Number(100)
-console.log(balance);
+//console.log(balance);
+
+console.log(balance.toString().length)
+//console.log(balance.toFixed(2))
+
+const otherNumber=123.4232
+//console.log(otherNumber.toPrecision(3));
+
+const hundreds =100000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++Maths+++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.min(4,3,5,7));
+console.log(Math.max(4,3,5,7));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
